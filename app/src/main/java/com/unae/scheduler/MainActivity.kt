d4eb423e132a4fd52fa898eb8360210529b8d9fc
unae131 <yjhyhw8998@gmail.com>
2021-04-29 22:31:07 +0900

    Initial commit

---
+++ app/src/main/java/com/unae/scheduler/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.unae.scheduler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener(){
+            Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
